ca5f54edf34b884565730ebb3c2547664a309ad3
Aayush Pandey <aayushpandey1100@gmail.com>
2021-11-22 12:49:58 +0530

    added login function using firebase

--- TwoWaits/app/src/main/java/com/aayushpandey/loginapp/MainActivity.java
+++ TwoWaits/app/src/main/java/com/aayushpandey/loginapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.aayushpandey.loginapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView accountcreate;
+Button login;
+EditText username,passWord;
+    String emailPattern="^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+ProgressDialog progressDialog;
+FirebaseAuth mAuth;
+FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accountcreate=findViewById(R.id.accountcreate);
+        accountcreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,registration.class);
+                startActivity(intent);
+
+            }
+        });
+
+        login=findViewById(R.id.login);
+        username=findViewById(R.id.Username);
+        passWord=findViewById(R.id.Password);
+        progressDialog=new ProgressDialog(this);
+        mAuth= FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestLogin();
+            }
+
+
+        });
+    }
+
+    private void requestLogin() {
+        String Email = username.getText().toString();
+        String password = passWord.getText().toString();
+
+        if (!Email.matches(emailPattern)) {
+            username.setError("Enter correct Email");
+
+        } else if (password.isEmpty() || password.length() < 6) {
+            passWord.setError("Enter proper password");
+        } else {
+            progressDialog.setMessage("Logging in...");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+            mAuth.signInWithEmailAndPassword(Email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(MainActivity.this, "Logged in!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
